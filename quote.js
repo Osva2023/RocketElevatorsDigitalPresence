9f505b2a56ab2c8e958e2671e26fbdc463066f61
Osvaldo Muguercia <elosvadel84@gmail.com>
2024-01-15 12:23:22 -0500

    i add the form with all fields, just the radio buttons are visible now

---
+++ quote.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const residentialRadio = document.getElementById('residential'); // residential radio
+    const commercialRadio = document.getElementById('commercial'); // commercial radio
+    const industrialRadio = document.getElementById('industrial'); // industrial  radio
+})
+
+
